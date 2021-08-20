d75d8b13086fe6cfd5c5df34a7efe4513d9ef090
Bassam Ramadan <bassamramadan964@gmail.com>
2021-08-20 13:48:56 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll',function(){
+
+header =document.querySelector('header');
+header.classList.toggle("sticky",window.scrollY > 0);
+
+
+})
+
+
+function menuToggle(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
